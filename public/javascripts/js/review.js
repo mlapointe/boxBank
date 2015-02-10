1469c2d9b8757b2361839767e87ad39c7359228c
Mike Lapointe <muzicmike@gmail.com>
2015-02-10 15:28:16 -0600

    Added dummy buttons & placeholder Box viewer

--- public/javascripts/js/review.js
+++ public/javascripts/js/review.js
@@ -1,7 +1,23 @@
 
+function confirmDownload(){
+
+  return confirm('Final view would reload viewer on this page. \n (see source for example code) \n Would you like to download file instead?')
+}
+
+function addComment(){
+  return alert('To be implemented after viewer above is live')
+}
+
+function acceptApplication(){
+
+  return alert('To be implemented after DB is created')
+
+}
 
 
 //Load file from viewer --> implement later
+//(need DB to store applications w/ Viewer ID -- Cannot expect file to process as soon as it is uploaded to the Box View API)
+
 // function selectFileToView(file){
 //
 //   console.log(file)
